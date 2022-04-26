8b62c5591eca533d0bb65247b30c9c5616d6b68c
Bekir - CDI <b.burakcebecii@gmail.com>
2022-04-26 12:58:11 +0200

    reading from data.json and loading to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,80 @@
 import "./App.scss";
+import _jobs from "./data/jobs.json";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">Hello!</div>;
+  const [displayKind, setDisplayKind] = useState("");
+  const [jobs, setJobs] = useState([]);
+
+  const saveToLocalStorage = () => {
+    const jobAppState = {
+      displayKind,
+      jobs,
+    };
+    localStorage.setItem("jobAppState", JSON.stringify(jobAppState));
+  };
+
+  const loadFromLocalStorage = () => {
+    const jobAppState = JSON.parse(localStorage.getItem("jobAppState"));
+    // updateWithJsonFile(_jobs, jobAppState);
+    if (jobAppState === null) {
+      setDisplayKind("full");
+      setJobs(_jobs);
+    } else {
+      setDisplayKind(jobAppState.displayKind);
+      setJobs(jobAppState.jobs);
+    }
+  };
+
+  const loadTechItems = () => {
+    (async () => {
+      const response = await fetch(techItemsUrl);
+      let _techItems = await response.json();
+      setTechItems(_techItems);
+    })();
+  };
+
+  useEffect(() => {
+    loadTechItems();
+    loadFromLocalStorage();
+  }, []);
+
+  useEffect(() => {
+    saveToLocalStorage();
+  }, [displayKind, jobs]);
+
+  const handleToggleView = () => {
+    const _displayKind = displayKind === "full" ? "list" : "full";
+    setDisplayKind(_displayKind);
+  };
+
+  const handleStatusChange = (job) => {
+    let statusIndex = statuses.indexOf(job.status);
+    statusIndex++;
+    if (statusIndex > statuses.length - 1) {
+      statusIndex = 0;
+    }
+    job.status = statuses[statusIndex];
+    setJobs([...jobs]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Job Application Process</h1>
+      <button className="toggle" onClick={handleToggleView}>
+        Toggle View
+      </button>
+      {displayKind === "full" ? (
+        <JobsFull
+          jobs={jobs}
+          handleStatusChange={handleStatusChange}
+          techItems={techItems}
+        />
+      ) : (
+        <JobsList jobs={jobs} />
+      )}
+    </div>
+  );
 }
 
 export default App;
